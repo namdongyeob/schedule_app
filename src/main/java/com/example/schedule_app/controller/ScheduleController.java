e18e46daea991ef844fc1ee457fc48da0b9eee0c
namdongyeob <ehdduq1324@gmail.com>
2026-04-18 22:44:57 +0900

    feat: Lv8 일정 페이징 조회 구현

--- src/main/java/com/example/schedule_app/controller/ScheduleController.java
+++ src/main/java/com/example/schedule_app/controller/ScheduleController.java
@@ -4,6 +4,7 @@ import com.example.schedule_app.dto.*;
 import com.example.schedule_app.service.ScheduleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,5 +42,11 @@ public class ScheduleController {
             @Valid @RequestBody DeleteScheduleRequest request){
         scheduleService.delete(scheduleId, request);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+    @GetMapping("/page")
+    public ResponseEntity<Page<SchedulePageResponse>> getPage(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findAllWithPage(page, size));
     }
 }
